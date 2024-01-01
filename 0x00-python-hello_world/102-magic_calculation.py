d114e0dd3edb0b709619e4e7116f5fe78c156246
emanbarhoma30 <emanahmedb92@gmail.com>
2024-01-01 15:42:14 -0800

    advanced

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    result = 98 + a ** b
+    return result
+
+# Example usage:
+# Uncomment the next two lines if you want to test the function
+# a_value = 2
+# b_value = 3
+# print(magic_calculation(a_value, b_value))
