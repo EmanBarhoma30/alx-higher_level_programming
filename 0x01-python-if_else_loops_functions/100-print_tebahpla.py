c1897a22c187d293d56308b3a207a90aed1557ae
emanbarhoma30 <emanahmedb92@gmail.com>
2024-01-02 14:48:03 -0800

    14. Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('Z'), ord('A') - 1, -1):
-    diff = 0 if i % 2 == 0 else 32
-    print("{:c}".format(i - diff), end='')
+for i in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format(i - 32 if i % 2 == 0 else i), end='')
