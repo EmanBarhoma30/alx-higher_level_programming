6b3f7c6a8895309765c0f0923079e75c50e0ffdb
emanbarhoma30 <emanahmedb92@gmail.com>
2024-01-02 10:47:12 -0800

    try to fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,12 @@ last_digit = abs(number) % 10
 last_digit *= -1 if number < 0 else 1  # Correcting the sign for negative numbers
 
 output_string = "Last digit of {:d} is {:d} and is".format(number, last_digit)
-output_string += " greater than 5" if last_digit > 5 else " 0" if last_digit == 0 else " less than 6 and not 0"
+
+if last_digit > 5:
+    output_string += " greater than 5"
+elif last_digit == 0:
+    output_string += " 0"
+else:
+    output_string += " less than 6 and not 0"
+
 print(output_string)
