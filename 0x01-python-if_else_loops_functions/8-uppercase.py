b1db1679708b5e583ead7753e123e4cca595d0cf
emanbarhoma30 <emanahmedb92@gmail.com>
2024-01-02 12:30:05 -0800

    8. To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-
-def uppercase(some_str):
-    for char in some_str:
-        # Check if the character is lowercase
-        if 97 <= ord(char) <= 122:
-            # Convert the lowercase character to uppercase using ASCII values
-            upper_char = chr(ord(char) - 32)
-            # Print the uppercase character without a newline
-            print(upper_char, end="")
-        else:
-            # Print characters other than lowercase letters
-            print(char, end="")
-    
-    # Print a newline after processing the string
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
     print()
-
-# Example usage:
-uppercase("best")
-uppercase("Best School 98 Battery street")
