f49bfdb9f27ed59f7b90da283111a1cd28a86abe
emanbarhoma30 <emanahmedb92@gmail.com>
2024-01-04 12:37:57 -0800

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div as calculator_function
-
-    a = 10
-    b = 5
-
-    result_add = calculator_function(a, b)
-    result_sub = sub(a, b)
-    result_mul = mul(a, b)
-    result_div = calculator_function(a, b)
-
-    print(f"{a} + {b} = {result_add}")
-    print(f"{a} - {b} = {result_sub}")
-    print(f"{a} * {b} = {result_mul}")
-    print(f"{a} / {b} = {result_div}")
+    from calculator_1 import add, sub, mul, div
+    x = 10
+    y = 5
+    print("{} + {} = {}".format(x, y, add(x, y)))
+    print("{} - {} = {}".format(x, y, sub(x, y)))
+    print("{} * {} = {}".format(x, y, mul(x, y)))
+    print("{} / {} = {}".format(x, y, div(x, y)))
