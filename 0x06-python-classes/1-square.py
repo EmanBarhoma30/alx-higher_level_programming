3c33f31e2e3f4abee161aae35ce7d8fe9831bb5c
emanbarhoma30 <emanahmedb92@gmail.com>
2024-01-23 12:30:28 -0800

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Module: Definition of a Square class.
+"""
+
+class Square:
+    """
+    Class: Square
+    Description: Represents a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
